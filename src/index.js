e6327dda9031afc30c7918ed7fdf43ed085b7a80
Gautier Miquet <xisabla.dev@gmail.com>
2019-01-21 23:47:40 +0100

    first commit

---
+++ src/index.js
@@ -0,0 +1,20 @@
+const Phaser = require('phaser');
+
+function create ()
+{
+    let text1 = this.add.text(100, 100, 'Hello World');
+    text1.setTint(0xffffff, 0xffffff, 0xffffff, 0xffffff);
+}
+
+let config = {
+    type: Phaser.WEBGL,
+    parent: 'phaser-exmaple',
+    width: 800,
+    height: 600,
+    scene: {
+        create: create
+    }
+};
+
+// eslint-disable-next-line no-unused-vars
+let game = new Phaser.Game(config);
