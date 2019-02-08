78538907ba89eb30fe5bbbeed2476e90bf33204c
Gautier Miquet <xisabla.dev@gmail.com>
2019-02-08 16:09:41 +0100

    feat(boot): use webfontloader for loading fonts in phaser

--- src/Scenes/Boot.js
+++ src/Scenes/Boot.js
@@ -1,4 +1,5 @@
 import Phaser from 'phaser';
+import WebFont from 'webfontloader';
 
 export default class extends Phaser.Scene
 {
@@ -9,27 +10,24 @@ export default class extends Phaser.Scene
         this.loaded = false;
     }
 
-    log (text)
-    {
-        if (!this.logText) this.logText = this.add.text(50, 100, '');
-
-        this.logText.setText(this.logText._text + '\n' + text);
-    }
-
     preload ()
     {
-        this.log('Using assets library: "http://labs.phaser.io"\n');
-        this.load.setBaseURL('http://labs.phaser.io');
+        this.add.text(100, 100, 'loading assets...');
 
-        this.log('Loading [apple]=[library:assets/sprites/apple.png]...');
+        this.load.setBaseURL('http://labs.phaser.io');
         this.load.image('apple', 'assets/sprites/apple.png');
 
-        this.log('\n\n\n\n\nWaiting 2s...');
+        this.add.text(100, 130, 'loading fonts...');
 
-        setTimeout(() =>
-        {
-            this.loaded = true;
-        }, 2000);
+        WebFont.load({
+            google: {
+                families: ['Raleway']
+            },
+            active: () =>
+            {
+                this.loaded = true;
+            }
+        });
     }
 
     update ()
