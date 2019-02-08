4de329e8b27b07615f1202860834a7f1d6910552
Gautier Miquet <xisabla.dev@gmail.com>
2019-02-08 15:42:33 +0100

    feat(hitboxes): add hitbox example

--- src/Scenes/Splash.js
+++ src/Scenes/Splash.js
@@ -1,6 +1,8 @@
 import Phaser from 'phaser';
 import Apple from '../sprites/Apple';
 
+import { Hitbox, renderHitboxes, updateHitboxes } from '../Engine/Hitbox';
+
 export default class extends Phaser.Scene
 {
     constructor ()
@@ -10,16 +12,34 @@ export default class extends Phaser.Scene
 
     create ()
     {
+        let tamere = new Hitbox({
+            label: 'body',
+            x: 0,
+            y: 0,
+            width: 80,
+            height: 80,
+            type: 'hitbox'
+        });
+
         this.add.text(400, 50, '~-[ CombaTiment ]-~', {
             font: '32px Bangers',
             fill: '#ffffff'
         }).setFontSize(20).setOrigin(0.5);
 
         this.apple = new Apple(this, 550, 50);
+        this.apple.hitboxes = {
+            active: 'still',
+            'still': [tamere]
+        };
+
+        this.graphics = this.add.graphics();
 
         this.add.existing(this.apple);
     }
 
     update ()
-    {}
+    {
+        updateHitboxes(this.apple);
+        renderHitboxes(this.graphics, [this.apple.hitboxes[this.apple.hitboxes.active]]);
+    }
 }
