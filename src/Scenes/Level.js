e44d5edbef5568c0b038ad3e591275e4c2a02a15
PaulBucampISEN <paul.bucamp@isen.yncrea.fr>
2019-03-22 14:59:28 +0100

    feat(Level): add base code for Level

--- src/Scenes/Level.js
+++ src/Scenes/Level.js
@@ -1,14 +1,13 @@
 import Phaser from 'phaser';
 
 import Character from '../Sprites/Character';
-import LevelPanelCollection from '../UI/LevelPanelCollection';
 import { updateHitboxes, renderHitboxes} from '../Engine/Hitbox'
 
 export default class extends Phaser.Scene
 {
     constructor ()
     {
-        super({ key: 'LevelSelect' });
+        super({ key: 'Level' });
     }
 
     init ()
@@ -16,20 +15,14 @@ export default class extends Phaser.Scene
 
     create ()
     {
+        this.physics.world.bounds.width = 5000;
+        this.physics.world.bounds.height = 900;
+
         this.add.image(800, 450, 'levelselect/background');
 
         this.ground = this.physics.add.staticGroup();
         this.ground.create(800, 810, 'levelselect/ground');
         this.add.image(800, 710, 'levelselect/grass');
-
-        this.panels = new LevelPanelCollection(this, { height: 550, y: 50, offset: 50 });
-
-        this.panels.add({ color: 0xf39c12, name: 'Consommation\nd\'énergie', power: 'power/thunder', ennemies: ['ennemy/apple', 'ennemy/apple', 'ennemy/apple', 'ennemy/apple', 'ennemy/apple', 'ennemy/apple', 'ennemy/apple', 'ennemy/apple', 'ennemy/apple'] });
-        this.panels.add({ color: 0xe74c3c, name: 'Consommation\nd\'énergie', power: 'power/thunder', ennemies: ['ennemy/apple', 'ennemy/apple', 'ennemy/apple'] });
-        this.panels.add({ color: 0x3498db, name: 'Consommation\nd\'énergie', power: 'power/thunder', ennemies: ['ennemy/apple', 'ennemy/apple', 'ennemy/apple', 'ennemy/apple'] });
-        this.panels.add({ color: 0x2ecc71, name: 'Consommation\nd\'énergie', power: 'power/thunder', ennemies: ['ennemy/apple', 'ennemy/apple'] });
-
-        this.panels.show(true, 200);
 
         this.player = new Character(this, 40, 500, 'feilong/idle', this.ground, 'feilong/hitbox', 'feilong');
         this.player.createAnim(this, 'idle', this.anims.generateFrameNumbers('feilong/idle', { start: 0, end: 10 }), 10, -1);
@@ -42,26 +35,14 @@ export default class extends Phaser.Scene
             enter: this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.Z)
         };
 
-        this.lastBouncedPanel = -1;
-        this.selectedPanel = -1;
-
         this.hitboxGraphics = this.add.graphics();
+
+        this.cameras.main.setBounds(0, 0, 5000, 900);
+        this.cameras.main.startFollow(this.player.body);   
     }
 
-    update (time)
+    update ()
     {
-        // Avoid crash: wait until the show animation is done
-        if (time >= this.panels.panels.length * 200 * 2 * 1.5)
-        {
-            this.panels.checkActions(this.player, this.keys);
-        }
-
-        if (this.keys.enter.isDown && this.panels.selected !== -1)
-        {
-            console.log('Go to level: ' + this.panels.selected);
-            this.scene.start('Level');
-        }
-
         if(this.player.anims.currentAnim !== null && this.player.anims.currentAnim.key === 'punch')
         {
             //if the punch animation has been triggered
