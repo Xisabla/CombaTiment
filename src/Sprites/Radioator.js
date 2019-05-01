9f721f41207e66dfebe9ad3cc10e44ba8a224753
Gautier Miquet <xisabla.dev@gmail.com>
2019-05-01 18:27:59 +0200

    feat,assets: add redatior enemy

---
+++ src/Sprites/Radioator.js
@@ -0,0 +1,22 @@
+import Enemy from './Enemy';
+
+export default class Radiator extends Enemy
+{
+    constructor (scene, x, y, ground)
+    {
+        super(
+            scene, 'radiator', x, y,
+            'enemies/radiator',
+            ground,
+            'enemies/radiator/hitbox', 'radiator',
+            {
+                'idle': { 'anim': scene.anims.generateFrameNumbers('enemies/radiator', { start: 0, end: 0 }), 'framerate': 10 },
+                'walk': { 'anim': scene.anims.generateFrameNumbers('enemies/radiator', { start: 0, end: 3 }), 'framerate': 10 },
+                'attack': { 'anim': scene.anims.generateFrameNumbers('enemies/radiator', { start: 4, end: 14 }), 'framerate': 10 }
+            },
+            { scale: 0.5 }
+        );
+
+        this.reverseFlipX = true;
+    }
+}
